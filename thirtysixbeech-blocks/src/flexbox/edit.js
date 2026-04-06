58a5bfc82b1ee7a20583912014cd7472aa1c0d9a
Aaron Weidele <aweidele@gmail.com>
2026-04-05 23:47:21 -0400

    Stack component options

--- thirtysixbeech-blocks/src/flexbox/edit.js
+++ thirtysixbeech-blocks/src/flexbox/edit.js
@@ -3,7 +3,7 @@
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
  */
-import { __ } from '@wordpress/i18n';
+import { __ } from "@wordpress/i18n";
 
 /**
  * React hook that is used to mark the block wrapper element.
@@ -11,10 +11,28 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useBlockProps, InnerBlocks, InspectorControls } from "@wordpress/block-editor";
-import { PanelBody, ToolbarButton, SelectControl, RangeControl, ToggleControl } from "@wordpress/components";
-import { justifyLeft, justifyCenter, justifyRight, justifySpaceBetween } from "@wordpress/icons";
-import { justifyTop, justifyCenterVertical, justifyBottom, justifyStretch } from "@wordpress/icons";
+import {
+	useBlockProps,
+	InnerBlocks,
+	InspectorControls,
+} from "@wordpress/block-editor";
+import {
+	PanelBody,
+	ToolbarButton,
+	RangeControl,
+	ToggleControl,
+	BaseControl,
+} from "@wordpress/components";
+
+import { Stack } from "@wordpress/ui";
+
+import { mobile, tablet, desktop } from "@wordpress/icons";
+
+const breakpoints = [
+	{ label: "Mobile", value: "all", icon: mobile },
+	{ label: "Tablet", value: "md", icon: tablet },
+	{ label: "Desktop", value: "lg", icon: desktop },
+];
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -22,7 +40,7 @@ import { justifyTop, justifyCenterVertical, justifyBottom, justifyStretch } from
  *
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
-import './editor.scss';
+import "./editor.scss";
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -32,13 +50,107 @@ import './editor.scss';
  *
  * @return {Element} Element to render.
  */
-export default function Edit( { attributes, setAttributes } ) {
-  const { evenColumns, reverse, justifyContent = "justify-start", alignItems = "items-start", breakpoint = "md", gap = "sm" } = attributes;
+
+import {
+	flexClasses,
+	justification,
+	alignment,
+} from "@shared/flex-grid-classes";
+
+export default function Edit({ attributes, setAttributes }) {
+	const {
+		evenColumns,
+		reverse,
+		justifyContent = "tsb-j-start",
+		alignItems = "tsb-a-start",
+		breakpoint = "md",
+		gap = "sm",
+	} = attributes;
+
+	const innerClasses = [flexClasses[breakpoint], "tsb-inner-blocks", justifyContent, alignItems];
+	if( reverse ) innerClasses.push( 'tsb-flex-row-r' );
+
 	return (
-		<div { ...useBlockProps() }>
-			<div className='tsb-flex tsb-inner-blocks'>
-				<InnerBlocks />
+		<>
+			<InspectorControls>
+				<PanelBody title={__("Layout Options", "thirtysix-beech")}>
+					<Stack direction="column">
+						<ToggleControl
+							label={__("Even Columns", "thirtysix-beech")}
+							checked={evenColumns}
+							help={__("All flex items will be equal width", "thirtysix-beech")}
+							onChange={(newValue) => {
+								setAttributes({
+									evenColumns: newValue,
+								});
+							}}
+						/>
+						<ToggleControl
+							label={__("Reverse", "thirtysix-beech")}
+							checked={reverse}
+							help={__("Reverse on mobile devices.")}
+							onChange={(newValue) => {
+								setAttributes({
+									reverse: newValue,
+								});
+							}}
+						/>
+						<BaseControl label="Justify Content">
+							<Stack alignment="center">
+								{justification.map((item) => (
+									<ToolbarButton
+										icon={item.icon}
+										label={item.label}
+										isPressed={justifyContent === item.value}
+										onClick={() =>
+											setAttributes({
+												justifyContent: item.value,
+											})
+										}
+									/>
+								))}
+							</Stack>
+						</BaseControl>
+
+						<BaseControl label="Align Items">
+							<Stack alignment="center">
+								{alignment.map((item) => (
+									<ToolbarButton
+										icon={item.icon}
+										label={item.label}
+										isPressed={alignItems === item.value}
+										onClick={() =>
+											setAttributes({
+												alignItems: item.value,
+											})
+										}
+									/>
+								))}
+							</Stack>
+						</BaseControl>
+
+						<BaseControl label="Breakpoints">
+							<Stack alignment="center">
+								{breakpoints.map((item) => (
+									<ToolbarButton
+										icon={item.icon}
+										label={item.label}
+										isPressed={breakpoint === item.value}
+										onClick={() =>
+											setAttributes({
+												breakpoint: item.value,
+											})
+										}
+									/>
+								))}
+							</Stack>
+						</BaseControl>
+					</Stack>
+				</PanelBody>
+			</InspectorControls>
+			<div {...useBlockProps( { className: innerClasses.join(" ") })}>
+					<InnerBlocks />
 			</div>
-		</div>
+		</>
 	);
 }
---
+++ thirtysixbeech-blocks/src/shared/flex-grid-classes.js
@@ -0,0 +1,75 @@
+import { justifyLeft, justifyCenter, justifyRight, justifySpaceBetween, justifyTop, justifyCenterVertical, justifyBottom, justifyStretch } from "@wordpress/icons";
+
+export const justifySpaceAround = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    {/* Left edge line */}
+    <path d="M3 4h2v16H3V4z" />
+    {/* Right edge line */}
+    <path d="M19 4h2v16h-2V4z" />
+    {/* Left block */}
+    <path d="M7 9h3v6H7V9z" />
+    {/* Right block */}
+    <path d="M14 9h3v6h-3V9z" />
+  </svg>
+);
+
+const flexClasses = {
+	all: "tsb-flex",
+	sm: "sm:tsb-flex",
+	md: "md:tsb-flex",
+	lg: "lg:tsb-flex",
+	xl: "xl:tsb-flex",
+};
+
+const justification = [
+	{
+		label: "Justify items left",
+		icon: justifyLeft,
+		value: "tsb-j-start",
+	},
+	{
+		label: "Justify items center",
+		icon: justifyCenter,
+		value: "tsb-j-center",
+	},
+	{
+		label: "Justify items right",
+		icon: justifyRight,
+		value: "tsb-j-end",
+	},
+	{
+		label: "Justify items space between",
+		icon: justifySpaceBetween,
+		value: "tsb-j-between",
+	},
+	{
+		label: "Justify items space around",
+		icon: justifySpaceAround,
+		value: "tsb-j-around",
+	},
+];
+
+const alignment = [
+	{
+		label: "Align items top",
+		icon: justifyTop,
+		value: "tsb-a-start",
+	},
+	{
+		label: "Align items center",
+		icon: justifyCenterVertical,
+		value: "tsb-a-center",
+	},
+	{
+		label: "Align items bottom",
+		icon: justifyBottom,
+		value: "tsb-a-end",
+	},
+	{
+		label: "Align items stretch",
+		icon: justifyStretch,
+		value: "tsb-a-stretch",
+	},
+];
+
+export { flexClasses, justification, alignment };
