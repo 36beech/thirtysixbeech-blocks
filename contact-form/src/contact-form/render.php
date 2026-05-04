995b1ee20466159139e3139ee1193619d348fdb5
Aaron Weidele <aweidele@gmail.com>
2026-05-03 20:27:46 -0400

    Contact form

--- contact-form/src/contact-form/render.php
+++ contact-form/src/contact-form/render.php
@@ -7,13 +7,20 @@ $id = uniqid();
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?>>
   <form>
-    <label for="<?php echo $id; ?>-email">Your email</label>
-    <input id="<?php echo $id; ?>-email" type="email" name="email" />
-    <label for="<?php echo $id; ?>-name">Your name</label>
-    <input id="<?php echo $id; ?>-name" type="text" name="name" />
-
-    <label for="<?php echo $id; ?>-message">Message</label>
-    <textarea id="<?php echo $id; ?>-message" name="message"></textarea>
-    <div class="wp-block-button"><button class="wp-block-button__link wp-element-button">Send</button></div>
+    <div class="contact-form flex flex-col gap-5">
+      <div>
+        <label class="contact-form-label" for="<?php echo $id; ?>-name">Your name</label>
+        <input id="<?php echo $id; ?>-name" type="text" name="name" />
+      </div>
+      <div>
+        <label class="contact-form-label" for="<?php echo $id; ?>-email">Your email</label>
+        <input id="<?php echo $id; ?>-email" type="email" name="email" />
+      </div>
+      <div>
+        <label class="contact-form-label" for="<?php echo $id; ?>-message">Message</label>
+        <textarea id="<?php echo $id; ?>-message" name="message"></textarea>
+      </div>
+      <div class="wp-block-button"><button class="wp-block-button__link wp-element-button">Send</button></div>
+    </div>
   </form>
 </div>
