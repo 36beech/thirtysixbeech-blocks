c687eb11b589d580b603f562f9694d2a30ba0016
Aaron Weidele <aweidele@gmail.com>
2026-05-11 22:42:01 -0400

    alignment

--- thirtysixbeech-blocks/src/grid/render.php
+++ thirtysixbeech-blocks/src/grid/render.php
@@ -4,14 +4,14 @@
  */
 
 $breakpoint = $attributes['breakpoint'];
-$breakpoint_classes = [
+$breakpoint_classes = array(
 	"mobile" => "max-sm:flex max-sm:flex-col sm:grid",
 	"tablet" => "max-md:flex max-md:flex-col md:grid",
 	"desktop" => "max-lg:flex max-lg:flex-col lg:grid"
-];
+);
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?>>
-	<div class="<?php echo $breakpoint_classes[$breakpoint]; ?> grid-cols-12 gap-5">
+	<div class="<?php echo $breakpoint_classes[$breakpoint]; ?> grid-cols-12 gap-5 <?php echo $attributes["alignItems"]; ?>">
 		<?php echo $content; ?>
 	</div>
 </div>
