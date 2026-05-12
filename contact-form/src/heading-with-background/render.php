479945a73637f6f8f43108b49fe6ecb8bae52bf5
Aaron Weidele <aweidele@gmail.com>
2026-05-11 22:16:30 -0400

    block heading

---
+++ contact-form/src/heading-with-background/render.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PHP file to use when rendering the block type on the server to show on the front end.
+ *
+ * The following variables are exposed to the file:
+ *     $attributes (array): The block attributes.
+ *     $content (string): The block default content.
+ *     $block (WP_Block): The block instance.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+$text_content = $attributes["textContent"];
+?>
+<h5 <?php echo get_block_wrapper_attributes(); ?>>
+	<span><?php echo $text_content; ?></span>
+</h5>
