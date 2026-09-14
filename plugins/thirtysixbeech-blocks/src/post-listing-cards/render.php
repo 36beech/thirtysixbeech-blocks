b640412f00f5a03f9faf90536f2a68f3dc5a6c77
Aaron Weidele <aweidele@gmail.com>
2026-09-13 22:23:57 -0400

    featured post

--- plugins/thirtysixbeech-blocks/src/post-listing-cards/render.php
+++ plugins/thirtysixbeech-blocks/src/post-listing-cards/render.php
@@ -62,13 +62,32 @@ endforeach;
 	<?php if (!empty($featured_post)):
 		$card = makePostCard($featured_post, $show, $dateFormat, $datePrefix);
 	?>
-		<pre><?php print_r($card); ?></pre>
-		<div class="grid grid-cols-12 gap-tsb">
-			<div class="col-span-4">
-				Featured
+
+		<div class="flex flex-col md:grid grid-cols-12 md:items-center gap-tsb mb-30 tsb-card-horizontal">
+
+			<div class="md:col-span-7 lg:col-span-8 md:col-start-6 lg:col-start-5 row-start-1">
+				<div class="tsb-card__image">
+					<?php if (!empty($card['image'])): ?>
+						<img src="<?php echo $card['image']; ?>" class="block h-full w-full object-cover object-center" />
+					<?php endif; ?>
+				</div>
 			</div>
-			<div class="col-span-8">
-				Image
+
+			<div class="md:col-span-5 lg:col-span-4 md:col-start-1 lg:col-start-1 row-start-1">
+				<div class="tsb-card__body">
+					<?php if (!empty($card["eyebrow"])): ?>
+						<div class="tsb-card__eyebrow"><?php echo $card["eyebrow"]; ?></div>
+					<?php endif; ?>
+					<?php if (!empty($card['title'])): ?>
+						<h3 class="tsb-card__heading"><?php echo $card['title']; ?></h3>
+					<?php endif; ?>
+					<?php if (!empty($card['description'])): ?>
+						<p class="tsb-card__description"><?php echo nl2br($card['description']); ?></p>
+					<?php endif; ?>
+				</div>
+				<?php if (!empty($card['link'])):
+					echo $card['link'];
+				endif; ?>
 			</div>
 		</div>
 	<?php endif; ?>
